481d7db41306d7c41306c69f064c3a42c2015221
Saiful Islam <saifulislamsshakib@gmail.com>
2026-08-09 00:09:56 +0600

    Updated project

---
+++ removeDuplicateArray.js
@@ -0,0 +1,36 @@
+// function removeDuplicates(arr) {
+//   let left = 0;
+//   let right = 1;
+
+//   while (right < arr.length) {
+//     if (arr[left] === arr[right]) {
+//       right++;
+//     } else {
+//       left++;
+//       arr[left] = arr[right];
+//       right++;
+//     }
+//   }
+
+//   return left + 1;
+// }
+// console.log(removeDuplicates([1, 1, 2, 2, 3, 3, 4]));
+
+let arr = [1, 1, 2, 2, 3, 3, 4];
+
+let left = 0;
+let right = 1;
+
+while (right < arr.length) {
+  if (arr[left] === arr[right]) {
+    right++;
+  } else {
+    left++;
+    arr[left] = arr[right];
+    right++;
+  }
+}
+
+let uniqueArr = arr.slice(0, left + 1);
+
+console.log(uniqueArr);
