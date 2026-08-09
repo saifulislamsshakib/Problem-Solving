da4f141204da62045252a99d756dbc766254b657
Saiful Islam <saifulislamsshakib@gmail.com>
2026-08-10 00:14:36 +0600

    Updated project

--- twoSum.js
+++ twoSum.js
@@ -14,6 +14,18 @@
 
 // console.log(twoSum([2, 7, 11, 15], 9));
 
+// function twoSum(arr, target) {
+//   let seen = {};
+//   for (let i = 0; i < arr.length; i++) {
+//     let complement = target - arr[i];
+//     if (seen[complement] !== undefined) {
+//       return [seen[complement], i];
+//     }
+//     seen[arr[i]] = i;
+//   }
+// }
+// console.log(twoSum([3, 2, 4], 6));
+
 function twoSum(arr, target) {
   let seen = {};
   for (let i = 0; i < arr.length; i++) {
@@ -24,4 +36,4 @@ function twoSum(arr, target) {
     seen[arr[i]] = i;
   }
 }
-console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 2, 4], 5));
