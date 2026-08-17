98bd4e458d586af5da6901069d3af6cee1898d1e
Saiful Islam <saifulislamsshakib@gmail.com>
2026-08-17 19:44:01 +0600

    8-15-26

--- removeDuplicateArray.js
+++ removeDuplicateArray.js
@@ -16,21 +16,44 @@
 // }
 // console.log(removeDuplicates([1, 1, 2, 2, 3, 3, 4]));
 
-let arr = [1, 1, 2, 2, 3, 3, 4];
+// let arr = [1, 1, 2, 2, 3, 3, 4];
 
-let left = 0;
-let right = 1;
+// let left = 0;
+// let right = 1;
 
-while (right < arr.length) {
-  if (arr[left] === arr[right]) {
-    right++;
-  } else {
-    left++;
-    arr[left] = arr[right];
-    right++;
+// while (right < arr.length) {
+//   if (arr[left] === arr[right]) {
+//     right++;
+//   } else {
+//     left++;
+//     arr[left] = arr[right];
+//     right++;
+//   }
+// }
+
+// let uniqueArr = arr.slice(0, left + 1);
+
+// console.log(uniqueArr);
+
+function removeDuplicates(arr) {
+  let left = 0;
+  let right = 1;
+
+  while (right < arr.length) {
+    if (arr[left] === arr[right]) {
+      right++;
+    } else {
+      left++;
+      arr[left] = arr[right];
+      right++;
+    }
   }
+
+  let uniqueArr = arr.slice(0, left + 1);
+
+  return uniqueArr;
 }
 
-let uniqueArr = arr.slice(0, left + 1);
+let arr = [1, 1, 2, 2, 3, 3, 4];
 
-console.log(uniqueArr);
+console.log(removeDuplicates(arr));
