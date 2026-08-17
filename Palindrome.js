51b6c20acc19b75d225dc41c16e89d8a3c15a117
Saiful Islam <saifulislamsshakib@gmail.com>
2026-08-17 12:50:57 +0600

    8-17-26

--- Palindrome.js
+++ Palindrome.js
@@ -41,17 +41,38 @@
 // }
 // console.log(isPalindrome([5, 6, 6, 5]));
 
-function isPalindrome(arr) {
+// function isPalindrome(arr) {
+//   let left = 0;
+//   let right = arr.length - 1;
+//   while (left < right) {
+//     if (arr[left] !== arr[right]) {
+//       return false;
+//     }
+//     left++;
+//     right--;
+//   }
+//   return true;
+// }
+
+// console.log(isPalindrome([7, 8, 9, 8, 7]));
+
+var isPalindrome = function (x) {
+  let str = x.toString();
   let left = 0;
-  let right = arr.length - 1;
-  while (left < right) {
-    if (arr[left] !== arr[right]) {
+  let right = str.length - 1;
+
+  while (right > left) {
+    if (str[left] !== str[right]) {
       return false;
     }
+
     left++;
     right--;
   }
-  return true;
-}
 
-console.log(isPalindrome([7, 8, 9, 8, 6]));
+  return true;
+};
+
+console.log(isPalindrome(-121)); // false
+console.log(isPalindrome(10)); // false
+console.log(isPalindrome(121)); // true
